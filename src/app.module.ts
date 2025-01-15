eadf95858228b5c98c7809fd31e386aa7e01bf93
Erick Berdnaski <berdnaski1177@gmail.com>
2025-01-15 00:09:02 -0300

    feat: add ConfigModule to manage environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { MemberController } from './http/controllers/member.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { MembersRepository } from './repositories/members.repository';
 import { PrismaMembersRepository } from './prisma/repositories/prisma-members.repository';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   exports: [MembersRepository],
   controllers: [MemberController],
   providers: [

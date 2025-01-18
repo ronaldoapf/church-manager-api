c25a60c7a6d0b858ce3cbe152bbfe140afe030bc
ronaldoapf <ronaldo.alves.1997@gmail.com>
2025-01-18 01:43:35 -0300

    feat: ConfigModule configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
-import { MemberController } from './http/controllers/member.controller';
 import { PrismaService } from './prisma/prisma.service';
 import { MembersRepository } from './repositories/members.repository';
 import { PrismaMembersRepository } from './prisma/repositories/prisma-members.repository';
+import { MembersController } from './http/controllers/members';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+    validate: (env) => envSchema.parse(env),
+    isGlobal: true,
+  })],
   exports: [MembersRepository],
-  controllers: [MemberController],
+  controllers: [MembersController],
   providers: [
     PrismaService,
     {

4dfb208ed77fae9616a739cd06820c424ee8a7c2
ronaldoapf <ronaldo.alves.1997@gmail.com>
2025-01-18 01:43:17 -0300

    feat: creat zod-validation-pipe

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,25 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { ZodError, ZodSchema } from "zod";
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    try {
+      this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          message: 'Validation failed',
+          statusCode: 400,
+          errors: fromZodError(error)
+        })
+      }
+      console.log(error)
+      throw new BadRequestException('Validation failed');
+    }
+    return value
+  }
+
+}
